2b0fb8c9c0f2ea03f387dece442784801d3d3bfb
marianabamato <marianabamato@gmail.com>
2022-04-04 13:57:04 -0300

    preprocessing script

---
+++ scripts/preprocess.sh
@@ -0,0 +1,21 @@
+while read PUBCHEM_BIOASSAY_ID; do
+ 
+        for FEATURE_SET in descriptors morgan-1024 morgan-2048 mol2vec; do
+ 
+                mkdir -p data/preprocessing/$PUBCHEM_BIOASSAY_ID/$FEATURE_SET
+                cd data/preprocessing/$PUBCHEM_BIOASSAY_ID/$FEATURE_SET
+ 
+                bambu-preprocess
+                        --input ../../../raw/$PUBCHEM_BIOASSAY_ID.csv
+                        --output $PUBCHEM_BIOASSAY_ID_preprocess.csv
+                        --output-preprocessor $PUBCHEM_BIOASSAY_ID_preprocessor.pickle
+                        --feature-type $FEATURE_SET
+                        --mol2vec-model-path ../../../../mol2vec.pickle
+                        --train-test-split 0.75
+                        --undersample
+ 
+                cd ../../../..
+ 
+        done
+ 
+done < ../../../pubchem_ids
