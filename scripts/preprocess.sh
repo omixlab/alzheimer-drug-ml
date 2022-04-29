ce292f2bf472e16039a4dd289b888bb98031fc28
Mariana Amato <marianabamato@gmail.com>
2022-04-29 13:09:56 -0300

    updated preprocessing script

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -5,17 +5,9 @@ while read PUBCHEM_BIOASSAY_ID; do
                 mkdir -p data/preprocessing/$PUBCHEM_BIOASSAY_ID/$FEATURE_SET
                 cd data/preprocessing/$PUBCHEM_BIOASSAY_ID/$FEATURE_SET
  
-                bambu-preprocess
-                        --input ../../../raw/$PUBCHEM_BIOASSAY_ID.csv
-                        --output $PUBCHEM_BIOASSAY_ID_preprocess.csv
-                        --output-preprocessor $PUBCHEM_BIOASSAY_ID_preprocessor.pickle
-                        --feature-type $FEATURE_SET
-                        --mol2vec-model-path ../../../../mol2vec.pickle
-                        --train-test-split 0.75
-                        --undersample
- 
+                bambu-preprocess --input ../../../raw/$PUBCHEM_BIOASSAY_ID.csv --output $PUBCHEM_BIOASSAY_ID\_preprocess.csv --output-preprocessor $PUBCHEM_BIOASSAY_ID\_preprocessor.pickle --feature-type $FEATURE_SET --mol2vec-model-path ../../../../mol2vec.pickle --train-test-split 0.75 --undersample
                 cd ../../../..
  
         done
  
-done < ../../../pubchem_ids
+done < data/raw/pubchem_ids
